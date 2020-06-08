a9b414df83ba2b25f720fdf91fe62d8213f79ab0
Dmitry Butyugin <dmbutyugin@google.com>
2020-06-08 23:58:03 +0200

    smooth_axis: Changed configuration parameters to smoother_freq_x/y
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/extras/smooth_axis.py
+++ klippy/extras/smooth_axis.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2020  Dmitry Butyugin <dmbutyugin@google.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
+import logging, math
 import chelper
 
 MAX_ACCEL_COMPENSATION = 0.005
@@ -18,12 +18,22 @@ class SmoothAxis:
                 'damping_ratio_x', 0., minval=0., maxval=1.)
         self.damping_ratio_y = config.getfloat(
                 'damping_ratio_y', 0., minval=0., maxval=1.)
-        self.accel_comp_x = config.getfloat(
-                'accel_comp_x', 0., minval=0., maxval=MAX_ACCEL_COMPENSATION)
-        self.accel_comp_y = config.getfloat(
-                'accel_comp_y', 0., minval=0., maxval=MAX_ACCEL_COMPENSATION)
-        self.smooth_x = config.getfloat('smooth_x', 0., minval=0., maxval=.200)
-        self.smooth_y = config.getfloat('smooth_y', 0., minval=0., maxval=.200)
+        smooth_x = config.getfloat('smooth_x', 0., minval=0., maxval=.200)
+        smooth_y = config.getfloat('smooth_y', 0., minval=0., maxval=.200)
+        accel_comp_x = config.getfloat(
+                'accel_comp_x', (3. * smooth_x / (4. * math.pi))**2,
+                minval=0., maxval=MAX_ACCEL_COMPENSATION)
+        accel_comp_y = config.getfloat(
+                'accel_comp_y', (3. * smooth_y / (4. * math.pi))**2,
+                minval=0., maxval=MAX_ACCEL_COMPENSATION)
+        self.smoother_freq_x = config.getfloat(
+                'smoother_freq_x',
+                1. / (2. * math.pi * math.sqrt(accel_comp_x)),
+                minval=0.)
+        self.smoother_freq_y = config.getfloat(
+                'smoother_freq_y',
+                1. / (2. * math.pi * math.sqrt(accel_comp_y)),
+                minval=0.)
         self.stepper_kinematics = []
         self.orig_stepper_kinematics = []
         # Stepper kinematics code
@@ -52,25 +62,33 @@ class SmoothAxis:
             self.stepper_kinematics.append(sk)
             self.orig_stepper_kinematics.append(orig_sk)
         # Configure initial values
-        config_smooth_x = self.smooth_x
-        config_smooth_y = self.smooth_y
-        self.smooth_x = self.smooth_y = 0.
-        self._set_smoothing(self.damping_ratio_x, self.damping_ratio_y,
-                            self.accel_comp_x, self.accel_comp_y,
-                            config_smooth_x, config_smooth_y)
-    def _set_smoothing(self, damping_ratio_x, damping_ratio_y
-                       , accel_comp_x, accel_comp_y
-                       , smooth_x, smooth_y):
-        old_smooth_time = max(self.smooth_x, self.smooth_y) * .5
-        new_smooth_time = max(smooth_x, smooth_y) * .5
+        config_smoother_freq_x = self.smoother_freq_x
+        config_smoother_freq_y = self.smoother_freq_y
+        self.smoother_freq_x = self.smoother_freq_y = 0.
+        self._set_smoothing(config_smoother_freq_x, config_smoother_freq_y,
+                            self.damping_ratio_x, self.damping_ratio_y)
+    def _set_smoothing(self, smoother_freq_x, smoother_freq_y
+                       , damping_ratio_x, damping_ratio_y):
+        max_old_freq = max(self.smoother_freq_x, self.smoother_freq_y)
+        old_smooth_time = 1. / (3. * max_old_freq) if max_old_freq > 0 else 0.
+        max_new_freq = max(smoother_freq_x, smoother_freq_y)
+        new_smooth_time = 1. / (3. * max_new_freq) if max_new_freq > 0 else 0.
         self.toolhead.note_step_generation_scan_time(new_smooth_time,
                                                      old_delay=old_smooth_time)
-        self.smooth_x = smooth_x
-        self.smooth_y = smooth_y
+        self.smoother_freq_x = smoother_freq_x
+        self.smoother_freq_y = smoother_freq_y
         self.damping_ratio_x = damping_ratio_x
         self.damping_ratio_y = damping_ratio_y
-        self.accel_comp_x = accel_comp_x
-        self.accel_comp_y = accel_comp_y
+        if smoother_freq_x > 0:
+            accel_comp_x = 1. / (2. * math.pi * smoother_freq_x)**2
+            smooth_x = 2. / (3 * smoother_freq_x)
+        else:
+            accel_comp_x = smooth_x = 0.
+        if smoother_freq_y > 0:
+            accel_comp_y = 1. / (2. * math.pi * smoother_freq_y)**2
+            smooth_y = 2. / (3 * smoother_freq_y)
+        else:
+            accel_comp_y = smooth_y = 0.
         ffi_main, ffi_lib = chelper.get_ffi()
         for sk in self.stepper_kinematics:
             ffi_lib.smooth_axis_set_time(sk, smooth_x, smooth_y)
@@ -79,47 +97,20 @@ class SmoothAxis:
             ffi_lib.smooth_axis_set_accel_comp(sk, accel_comp_x, accel_comp_y)
     cmd_SET_SMOOTH_AXIS_help = "Set cartesian time smoothing parameters"
     def cmd_SET_SMOOTH_AXIS(self, gcmd):
-        damping_ratio_xy = gcmd.get_float(
-                'DAMPING_RATIO_XY', -1.0, minval=-1.0, maxval=1.)
-        if damping_ratio_xy < 0:
-            damping_ratio_x = gcmd.get_float(
-                    'DAMPING_RATIO_X', self.damping_ratio_x,
-                    minval=0., maxval=1.)
-            damping_ratio_y = gcmd.get_float(
-                    'DAMPING_RATIO_Y', self.damping_ratio_y,
-                    minval=0., maxval=1.)
-        else:
-            damping_ratio_x = damping_ratio_y = damping_ratio_xy
-        accel_comp_xy = gcmd.get_float(
-                'ACCEL_COMP_XY', -1.0,
-                minval=-1.0, maxval=MAX_ACCEL_COMPENSATION)
-        if accel_comp_xy < 0:
-            accel_comp_x = gcmd.get_float(
-                    'ACCEL_COMP_X', self.accel_comp_x,
-                    minval=0., maxval=MAX_ACCEL_COMPENSATION)
-            accel_comp_y = gcmd.get_float(
-                    'ACCEL_COMP_Y', self.accel_comp_y,
-                    minval=0., maxval=MAX_ACCEL_COMPENSATION)
-        else:
-            accel_comp_x = accel_comp_y = accel_comp_xy
-        smooth_xy = gcmd.get_float('SMOOTH_XY', -1.0,
-                                   minval=-1.0, maxval=.200)
-        if smooth_xy < 0:
-            smooth_x = gcmd.get_float('SMOOTH_X', self.smooth_x,
-                                      minval=0., maxval=.200)
-            smooth_y = gcmd.get_float('SMOOTH_Y', self.smooth_y,
-                                      minval=0., maxval=.200)
-        else:
-            smooth_x = smooth_y = smooth_xy
-        self._set_smoothing(damping_ratio_x, damping_ratio_y,
-                            accel_comp_x, accel_comp_y,
-                            smooth_x, smooth_y)
-        gcmd.respond_info("damping_ratio_x:%.9f damping_ratio_y:%.9f "
-                          "accel_comp_x:%.9f accel_comp_y:%.9f "
-                          "smooth_x:%.6f smooth_y:%.6f" % (
-                              damping_ratio_x, damping_ratio_y
-                              , accel_comp_x, accel_comp_y
-                              , smooth_x, smooth_y))
+        damping_ratio_x = gcmd.get_float(
+                'DAMPING_RATIO_X', self.damping_ratio_x, minval=0., maxval=1.)
+        damping_ratio_y = gcmd.get_float(
+                'DAMPING_RATIO_Y', self.damping_ratio_y, minval=0., maxval=1.)
+        smoother_freq_x = gcmd.get_float(
+                'SMOOTHER_FREQ_X', self.smoother_freq_x, minval=0.)
+        smoother_freq_y = gcmd.get_float(
+                'SMOOTHER_FREQ_Y', self.smoother_freq_y, minval=0.)
+        self._set_smoothing(smoother_freq_x, smoother_freq_y,
+                            damping_ratio_x, damping_ratio_y)
+        gcmd.respond_info("smoother_freq_x:%.3f smoother_freq_y:%.3f "
+                          "damping_ratio_x:%.6f damping_ratio_y:%.6f" % (
+                              smoother_freq_x, smoother_freq_y,
+                              damping_ratio_x, damping_ratio_y))
 
 def load_config(config):
     return SmoothAxis(config)
